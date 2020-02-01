7e17a6b4f1be0f685e70bc3fdef2a94d2272bb2e
Kobi <kobi.shasha@gmail.com>
2020-02-01 10:37:38 +0200

    table added

--- data/choice.ts
+++ data/choice.ts
@@ -4,7 +4,6 @@ export const CHOICES = [
         'value': 'Generate Home Component',
         'ts':
 `import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -18,5 +17,96 @@ export class HomeComponent {
 `,
         'css': 
 `/* styles */`
+    },
+    {
+        'tag': 'table',
+        'value': 'Generate Table Component',
+        'ts': `
+        import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  public date:Date
+  constructor() { }
+
+  ngOnInit() {
+    this.date = new Date();
+  }
+
+}
+`,
+        'html': `
+        <div class="header">
+    <img  src="https://www.johnbryce.co.il/src/jbh_small.jpg">
+    <h1>Shaked shhh......</h1>
+</div>
+<!-- 
+<div>
+    
+    <a href="#default" class="logo">Class 822.8 Haifa</a>
+</div>
+    
+        `,
+        'css': `
+        div{
+          height: 100%;
+          background-color: blue;
+          text-align: center;
+          font-family: 'Gill Sans';
+        }
+        
+        logoImage{
+          text-align: start;
+        }
+        
+        img{
+          width: 100px;
+          height: 50px;
+          top: 50%;
+          position: relative;
+          margin: 5px;
+          transform: translateY(-50%);
+          vertical-align: middle;
+          float: left;
+        }
+          
+        
+        a {
+            color: white;
+            text-decoration:dashed;
+        }
+        
+        a:hover 
+        {
+             color:rosybrown;
+             text-decoration:none; 
+             cursor:pointer;  
+        }
+        
+        p{
+          margin: 0;
+          position: relative;
+          top:50%;
+          transform: translateY(-50%);
+          font-size: large;
+          color: white;
+          
+        }
+        
+        h1{
+          margin: 0;
+          position: relative;
+          color: white;
+          top:50%;
+          font-size: 40px;
+          transform: translateY(-50%);
+          
+        }
+        ` 
     }
 ];
